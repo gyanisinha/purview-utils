d269a24e4ef6f6a79414f3a44dde8073572336a2
Gyani Sinha <87772005+gyanisinha@users.noreply.github.com>
2023-01-10 22:27:01 +0800

    Create bulk_create_entities_sqldw.py

---
+++ bulk_create_entities_sqldw.py
@@ -0,0 +1,24 @@
+import json
+from pyapacheatlas.auth import ServicePrincipalAuthentication
+from pyapacheatlas.core.client import PurviewClient
+from pyapacheatlas.readers import ExcelConfiguration, ExcelReader
+
+auth = ServicePrincipalAuthentication(
+    client_id="<enter value>",
+    tenant_id="<enter value>",    
+    client_secret="<enter value>"
+)
+client = PurviewClient(
+    account_name= "<enter value>",
+    authentication = auth
+)
+
+
+ec = ExcelConfiguration()
+reader = ExcelReader(ec)
+
+entities = reader.parse_bulk_entities("./BulkUploadEntities.xlsx")
+
+results = client.upload_entities(entities)
+
+print(json.dumps(results, indent=2))
